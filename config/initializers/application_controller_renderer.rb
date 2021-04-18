deefb1d9548ef4cd371bf7882c90d3f370d284af
ayakar338 <0pu2tr1478t396c@ezweb.ne.jp>
2021-04-18 08:13:03 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4dc918755a213b1f9524f0873f3dab9925e28523b7347fcd402a32aabdbded7777bc63ed1ffa6d704501c2502d9d6935edeebbd8b9ddacb31d85bc58918fd56a'
